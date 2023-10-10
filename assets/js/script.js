71bbcc0c81059a753706ba438ab0c56ee95c1ef8
olufsen <honeydrop@gmail.com>
2023-10-10 08:26:27 +0100

    finished header script

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,3 +37,20 @@ const navToggler = document.querySelector("[data-nav-toggler]");
 const toggleNavbar = function() { navbar.classList.toggle("active"); }
 
 navToggler.addEventListener("click", toggleNavbar);
+
+
+
+/**
+ * HEADER
+ * 
+ * active header when window scrolled to 50px
+ */
+
+const header = document.querySelector("[data-header]");
+
+const activeHeader = function() {
+    window.scrollY > 50 ? header.classList.add("active") :
+        header.classList.remove("active");
+}
+
+window.addEventListener("scroll", activeHeader);
