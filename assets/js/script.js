319274f8a38a40fb07bf0fe0b0b091c8aba042c0
olufsen <honeydrop@gmail.com>
2023-10-10 08:24:35 +0100

    fixed navbar toggle for mobile view

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,3 +33,7 @@ window.addEventListener("load", function() {
 
 const navbar = document.querySelector("[data-navbar]");
 const navToggler = document.querySelector("[data-nav-toggler]");
+
+const toggleNavbar = function() { navbar.classList.toggle("active"); }
+
+navToggler.addEventListener("click", toggleNavbar);
