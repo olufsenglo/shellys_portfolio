bf0d750c7c53524d7910c7c98e50b26109dcde26
olufsen <honeydrop@gmail.com>
2023-10-10 08:20:58 +0100

    fixed preloader javascript file

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+
+
+/**
+ * Add eventListener on multiple elements
+ */
+
+const addEventOnElements = function(elements, eventType, callback) {
+    for (let i = 0, len = elements.length; i < len; i++) {
+        elements[i].addEventListener(eventType, callback);
+    }
+}
+
+
+
+/**
+ * PRELOADER
+ */
+
+const preloader = document.querySelector("[data-preloader]");
+
+window.addEventListener("load", function() {
+    preloader.classList.add("loaded");
+    document.body.classList.add("loaded");
+});
